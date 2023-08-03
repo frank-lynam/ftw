df050a8c36b51e5cab8044eb9981d86242d12d02
Frank <lynamf@gmail.com>
2023-08-03 17:23:19 -0400

    Tidy

--- demo_one.py
+++
@@ -1,5 +0,0 @@
-def example_undecorated_file():
-  return True
-
-def i_can_haz_input(x=1):
-  return x
--- demo_two.py
+++
@@ -1,6 +0,0 @@
-def hidden_method(x, y):
-  return x+y
-
-@ftw
-def visible_method(x, y):
-  return hidden_method(x, y)
